7fd6068e5cd64208c189a5cd865cec8a49a50027
kalemat00 <kalemat@hotmail.it>
2022-12-21 12:30:30 +0100

    initial commit

---
+++ src/nativeMain/kotlin/Main.kt
@@ -0,0 +1,9 @@
+fun main() {
+    var variable1 : String = "gino"
+    var variable2 : Int = 345
+    var variable3 : Float = 2.654F
+
+    println("variable 1 : ${variable1}")
+    println("variable 2 : ${variable2}")
+    println("variable 3 : ${variable3}")
+}
